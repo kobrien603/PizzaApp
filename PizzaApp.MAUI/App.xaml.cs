01cb6bd275cdd4ea4c7f8831252b01b11539910a
Kieran O'Brien <kobrien603@gmail.com>
2022-11-02 20:19:53 -0400

    add MAUI project to solution

---
+++ PizzaApp.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaApp.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
