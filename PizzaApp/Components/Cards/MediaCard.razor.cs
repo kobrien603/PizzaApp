db193bc2eebfa0714aea1532edb992de3bce90e3
Kieran O'Brien <kobrien603@gmail.com>
2022-11-02 23:56:42 -0400

    start adding components. mobile friendly cards complete. Started working footers but still needs work

---
+++ PizzaApp/Components/Cards/MediaCard.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Components
+{
+	partial class MediaCard
+	{
+		[Parameter]
+		public string Image { get; set; } = "_content/PizzaApp/images/default.jpg";
+
+		[Parameter]
+		public string Title { get; set; } = string.Empty;
+
+		[Parameter] 
+		public string Message { get; set; } = string.Empty;
+
+		[Parameter]
+		public string RedirectURL { get; set; } = string.Empty;
+
+        [Parameter]
+        public string RedirectText { get; set; } = string.Empty;
+
+        [Parameter]
+		public int Height { get; set; } = 200;
+	}
+}
