3ece871727e35104457a4644f934e1767a9aa059
Kieran O'Brien <kobrien603@gmail.com>
2022-11-03 22:44:46 -0400

    basic creation for create account. will determine fields when backend is setup

---
+++ PizzaApp/Components/CreateAccountForm/CreateAccountForm.razor.cs
@@ -0,0 +1,42 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Components
+{
+    partial class CreateAccountForm
+    {
+        MudForm MudForm;
+        MudTextField<string> Password;
+        bool FormValid { get; set; }
+
+
+        private IEnumerable<string> PasswordStrength(string pw)
+        {
+            if (string.IsNullOrWhiteSpace(pw))
+            {
+                yield return "Password is required!";
+                yield break;
+            }
+            if (pw.Length < 8)
+                yield return "Password must be at least of length 8";
+            if (!Regex.IsMatch(pw, @"[A-Z]"))
+                yield return "Password must contain at least one capital letter";
+            if (!Regex.IsMatch(pw, @"[a-z]"))
+                yield return "Password must contain at least one lowercase letter";
+            if (!Regex.IsMatch(pw, @"[0-9]"))
+                yield return "Password must contain at least one digit";
+        }
+
+        private string PasswordMatch(string tmpPassword)
+        {
+            if (Password.Value != tmpPassword)
+                return "Passwords don't match";
+            return null;
+        }
+    }
+}
