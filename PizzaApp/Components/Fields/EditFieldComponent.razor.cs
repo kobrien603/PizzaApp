e35d1c91cac2b3fead8448c0aea064d712bf6756
Kieran O'Brien <kobrien603@gmail.com>
2023-02-21 21:26:05 -0500

    improve edit field component. now it dynamically reads TItem type and displays correct input box. Also add toggle to edit based on users input

--- PizzaApp/Components/Fields/EditFieldComponent.razor.cs
+++ PizzaApp/Components/Fields/EditFieldComponent.razor.cs
@@ -3,6 +3,7 @@ using MudBlazor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +16,18 @@ namespace PizzaApp.Components
         [Parameter] public string Name { get; set; }
         [Parameter] public TItem Value { get; set; }
         [Parameter] public EventCallback<TItem> UpdateValue { get; set; }
+
+        private bool EditMode = false;
+        private bool BtnEdit = false;
+
+        private async Task EditRow()
+        {
+            BtnEdit = true;
+
+            await Task.Delay(10);
+
+            EditMode = true;
+            BtnEdit = false;
+        }
     }
 }
